75ef9df6b1d108b40ac8027f1b16e2363bf36a74
Kalpesh Lambade <123794408+KalpeshLambade@users.noreply.github.com>
2023-09-30 18:19:06 +0530

    completed

--- catcare/lib/main.dart
+++ catcare/lib/main.dart
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
                       width: MediaQuery.of(context).size.width * 0.35,
                       decoration: const BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage('images/catdoc.png'),
-                            fit: BoxFit.cover,
-                            ),
+                          image: AssetImage('images/catdoc.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
@@ -85,6 +85,147 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: const Text(
+                      'About',
+                      style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15.0, vertical: 5.0),
+                    child: const Text(
+                      'Specializing exclusively in cat care, Dr. Meow has an uncanny ability to decipher the secret language of meows.Known for his gentle touch and his preference for feathered toy consultations.After all, a happy cat is a healthy cat, and with Dr. Meow, every appointment is a tail-wagging, purr-inducing experience! 😸',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 200.0,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 15.0, horizontal: 0),
+                    child: const Column(children: <Widget>[
+                      Card(
+                        child: ListTile(
+                          titleAlignment: ListTileTitleAlignment.center,
+                          leading: Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                          ),
+                          title: Text(
+                            'Address',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15.0),
+                          ),
+                          subtitle: Text(
+                              'Dr. Meow Purrfect Clinic,\n 123 Whisker \nPurringtonville, CA 98765 \n USA'),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          titleAlignment: ListTileTitleAlignment.center,
+                          leading: Icon(
+                            Icons.lock_clock,
+                            color: Colors.green,
+                          ),
+                          title: Text(
+                            'Daily Practice',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 15.0),
+                          ),
+                          subtitle: Text('Monday-Friday\n open till 7pm'),
+                        ),
+                      )
+                    ]),
+                  ),
+                  Container(
+                    height: 200.0,
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/map.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120.0,
+                child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text('Activity',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          color: Colors.black38,
+                          height: 80.0,
+                          width:MediaQuery.of(context).size.width*0.45,
+                          child:const Center(
+                            child:  ListTile(
+                              titleAlignment: ListTileTitleAlignment.center,
+                              leading: Icon(Icons.schedule,
+                              color: Colors.white,
+                              size: 30.0,),
+                              title: Text('List of Schedule',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0
+                              ),),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          color: Colors.white54,
+                          height: 80.0,
+                          width:MediaQuery.of(context).size.width*0.45,
+                          child: const Center(
+                            child: ListTile(
+                              titleAlignment: ListTileTitleAlignment.center,
+                              leading: Icon(Icons.card_membership,
+                              color: Colors.black,
+                              size: 30.0,),
+                              title: Text('Doctor Daily Post',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0
+                              ),),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
